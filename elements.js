af141f59fc4882faa48296bf29351d5cbfdc5829
pablo bracciale <pabloibracciale@gmail.com>
2022-05-25 11:34:20 -0300

    se agrega elements y ajuste css

---
+++ elements.js
@@ -0,0 +1,23 @@
+let personas=[
+    personaUno={
+      apellido: "Gomez",
+      nombre: "Roberto",
+      edad: 55
+    },
+    personaDos={
+      apellido: "Lopez",
+      nombre: "Agustin",
+      edad: 25
+    },
+    personaTres={
+      apellido: "Dominguez",
+      nombre: "Julian",
+      edad: 22
+    },
+    personaCuatro={
+        apellido: "Perez",
+        nombre: "Ramiro",
+        edad: 43
+      },
+      
+  ]
--- prueba.js
+++ prueba.js
@@ -1,30 +1,25 @@
-let cards=document.getElementsByClassName("cards");
-let personas=[
-  personaUno={
-    apellido: "Bracciale",
-    nombre: "Pablo",
-    edad: 43
-  },
-  personaDos={
-    apellido: "Bracciale",
-    nombre: "Pablo",
-    edad: 43
-  },
-  personaTres={
-    apellido: "Bracciale",
-    nombre: "Pablo",
-    edad: 43
-  }
-]
+let contenedor=document.getElementById("contenedor-cards");
 
 window.addEventListener("load", function(){
   personas.forEach(e => {
+    contenedor.classList.add("contenedor-cards");
     let card=document.createElement("div");
     let text=document.createElement("p");
+    let button=document.createElement("button");
+    button.innerText="Conoceme"
+    card.appendChild(button);
     card.appendChild(text);
-    text.innerText="Hola soy" + " " + e.apellido + " " + e.nombre + " " + "y tengo" + " " + e.edad;
-    cards[0].appendChild(card);
-    card.classList.add("fondo")
+    text.innerText="Hola soy" + " " + e.apellido + " " + e.nombre + " " + "y tengo" + " " + e.edad + " años";
+    contenedor.appendChild(card);
+    card.classList.add("card-persona");
+    text.classList.add("texto-persona");
+    button.classList.add("button")
+    text.classList.add("texto-margin");
+    button.addEventListener("click", function(){
+      text.classList.toggle("texto-persona");
+    })
   });
 });
+
+
 
